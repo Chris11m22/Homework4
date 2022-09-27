4ffcf992cc09b2c096ea60378ee9e7d04738a345
christinam <malininak11@gmail.com>
2022-09-27 20:55:16 +0500

    дз 2

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 13;
+        int age = 14;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием");
-        }
-        if (age < 18) ;{
+        } else {
             System.out.println("Возраст совершеннолетия ещё не наступил");
         }
 
-        if (age >= 7 && age <18); {
-            System.out.println("Ребенок может ходить в школу");
-        }
-        if (age >= 18 && age < 24) ;{
+        if (age >= 7 ) {
+            System.out.println("Может ходить в школу");
+        } else if (age >= 18 ) {
             System.out.println("Может пойти в университет");
-        }
-        if (age >= 24) ;{
+        } else if (age >= 24) {
             System.out.println("Может искать работу");
         }
+
+
         int capacity = 102;
         int seat = 60;
         int standingRoom = capacity - seat;
 
         int seatsUsed = 40;
-        int stabdingRoomUsed = 30;
+        int standingRoomUsed = 40;
 
-        if (seatsUsed < seat); {
+        if (seatsUsed < seat) {
             System.out.println("Осталось " + (seat - seatsUsed) + " сидячих мест");
-            if (standingRoom == stabdingRoomUsed);{
-                System.out.println("Стоячих мест не осталось");
+        } else {
+            if (seatsUsed == seat) {
+                System.out.println("Сидячих мест не осталось");
             }
         }
     }
